70201b0a8b5c7acb7405c504d525bc50ccb6933f
GDybas <grze.dybas@gmail.com>
2020-09-08 00:02:04 +0200

    Added random quotes api

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,41 +1,43 @@
 //Defined variables
 
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.tasks-list');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task');
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".tasks-list");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
 
-
-
-
+//Quotes Variables
+const quote = document.querySelector(".quote__text");
+const author = document.querySelector(".quote__author");
 
 loadEventListeners();
 
 function loadEventListeners() {
   //Loading saved data from LS
-  document.addEventListener('DOMContentLoaded', getTasks);
+  document.addEventListener("DOMContentLoaded", getTasks);
   //init date and time
-  document.addEventListener('DOMContentLoaded', getDate);
+  document.addEventListener("DOMContentLoaded", getDate);
   //init time
-  document.addEventListener('DOMContentLoaded', getTime);
+  document.addEventListener("DOMContentLoaded", getTime);
+  document.addEventListener("DOMContentLoaded", loadQuote);
+
   //Adding tasks
-  form.addEventListener('submit', addTask);
-  taskList.addEventListener('click', removeTask);
-  clearBtn.addEventListener('click', clearTasks);
+  form.addEventListener("submit", addTask);
+  taskList.addEventListener("click", removeTask);
+  clearBtn.addEventListener("click", clearTasks);
 }
 
 //Add Tasks
 function addTask(e) {
-  if (taskInput.value === '') {
-    alert('Add a task');
+  if (taskInput.value === "") {
+    alert("Add a task");
   }
 
-  const li = document.createElement('li');
-  li.className = 'tasks-list__item';
+  const li = document.createElement("li");
+  li.className = "tasks-list__item";
   li.appendChild(document.createTextNode(taskInput.value));
-  const link = document.createElement('a');
-  link.className = 'delete-item li-aside';
+  const link = document.createElement("a");
+  link.className = "delete-item li-aside";
   link.innerHTML = '<i class="fa fa-remove"></i>';
   li.appendChild(link);
 
@@ -43,14 +45,14 @@ function addTask(e) {
   //Store task in LS
   storeTaskInLocalStorage(taskInput.value);
 
-  taskInput.value = '';
+  taskInput.value = "";
   e.preventDefault();
 }
 
 //Remove task
 function removeTask(e) {
-  if (e.target.parentElement.classList.contains('delete-item')) {
-    if (confirm('Are You Sure?')) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are You Sure?")) {
       e.target.parentElement.parentElement.remove();
 
       //remove form LS
@@ -71,53 +73,65 @@ function clearTasks(e) {
 //Get Task from LS function
 function getTasks() {
   let tasks;
-  if (localStorage.getItem('tasks') === null) {
+  if (localStorage.getItem("tasks") === null) {
     tasks = [];
   } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks = JSON.parse(localStorage.getItem("tasks"));
   }
   tasks.forEach(task => {
-    const li = document.createElement('li');
-    li.className = 'tasks-list__item';
+    const li = document.createElement("li");
+    li.className = "tasks-list__item";
     li.appendChild(document.createTextNode(task));
-    const link = document.createElement('a');
-    link.className = 'delete-item li-aside';
+    const link = document.createElement("a");
+    link.className = "delete-item li-aside";
     link.innerHTML = '<i class="fa fa-remove"></i>';
     li.appendChild(link);
 
     taskList.appendChild(li);
-  })
+  });
 }
 
 //Store tasks in LS function
 function storeTaskInLocalStorage(task) {
   let tasks;
-  if (localStorage.getItem('tasks') === null) {
+  if (localStorage.getItem("tasks") === null) {
     tasks = [];
   } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks = JSON.parse(localStorage.getItem("tasks"));
   }
   tasks.push(task);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 //Remove task from local storage
 function removeTaskFromLocalStorage(taskItem) {
   let tasks;
-  if (localStorage.getItem('tasks') === null) {
+  if (localStorage.getItem("tasks") === null) {
     tasks = [];
   } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks = JSON.parse(localStorage.getItem("tasks"));
   }
   tasks.forEach((task, index) => {
     if (taskItem.textContent === task) {
-      tasks.splice(index, 1)
+      tasks.splice(index, 1);
     }
   });
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 //Clearing all tasks from LS
 function clearTasksFromLocalStorage() {
   localStorage.clear();
+}
+
+//Load quote ---------------------------------------------------------------
+function loadQuote(e) {
+  fetch("https://api.quotable.io/random")
+    .then(response => response.json())
+    .then(data => {
+      console.log(`${data.content} —${data.author}`);
+      quote.innerHTML = `${data.content}`;
+      author.innerHTML = `~${data.author}`;
+    });
+  e.preventDefault();
 }
